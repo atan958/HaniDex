4ec7815d240a15290d5f4a841485acdeac5fa63b
Angelo <atan958@aucklanduni.ac.nz>
2021-12-28 18:35:07 +1300

    Resized the missingno png.

--- src/js/Components/navBar/NavBar.js
+++ src/js/Components/navBar/NavBar.js
@@ -1,11 +1,9 @@
 import '../../../css/NavBar.css'
 
-const missingNo = require(`../../../pokemon-assets/assets/img/pokemon/missingno.png`)
-
 const NavBar = () => {
     return (
         <ul>
-            <li><a href="google.com" onClick={()=>console.log("missing No")}>HaniDex<img style={{height: '17px', marginLeft: '10px'}} src={missingNo}></img></a></li>
+            <li><a href="google.com" onClick={()=>console.log("missing No")}>HaniDex</a></li>
             <li><a href="news.asp">News</a></li>
             <li><a href="contact.asp">Contact</a></li>
             <li><a href="about.asp">About</a></li>
--- src/js/Utilities/PkmImgProvider.js
+++ src/js/Utilities/PkmImgProvider.js
@@ -5,8 +5,6 @@ const providePkmImg = (pkmName) => {
     }
     catch(e) {
         pkmPng = require(`../../pokemon-assets/assets/img/pokemon/missingno.png`);
-        //pkmImg = <img src={pkmPng} width="75" height="160" style={{ marginTop: '20px'}}></img>;
-        //console.log('error mayt ' + pkmName);
     }
     return pkmPng;
 }
