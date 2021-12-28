a8e67474a027638afe2d346f17f656de248386b1
Angelo <atan958@aucklanduni.ac.nz>
2021-12-28 22:33:07 +1300

    Added an overlay button for adding/removing pokemon.

--- src/js/Components/pokemonGrid/PkmItem.js
+++ src/js/Components/pokemonGrid/PkmItem.js
@@ -18,7 +18,10 @@ const PkmItem = ({ pkmData, addPkm, rmvPkm, atMaxNumPkm }) => {
     let pkmName = pkmData.name.charAt(0).toUpperCase() + pkmData.name.slice(1, pkmData.name.length);
 
     return (
-        <div className={`grid-item pkmItem-container fadeIn-animation ${pkmData.selected?'pkmItem-selected' : 'pkmItem-notSelected'} ${(atMaxNumPkm && !pkmData.selected) && 'cantSelectPkm'}`}  style={pkmData.selected? { backgroundColor: '#b44b4b' } : null} onClick={toggleSelected} onMouseOver={hoverOn} onMouseLeave={hoverOff}>
+        <div className={`container grid-item pkmItem-container fadeIn-animation ${pkmData.selected?'pkmItem-selected' : 'pkmItem-notSelected'} ${(atMaxNumPkm && !pkmData.selected) && 'cantSelectPkm'}`}  style={pkmData.selected? { backgroundColor: '#b44b4b' } : null} onMouseOver={hoverOn} onMouseLeave={hoverOff}>
+            <div className="overlay-div" onClick={toggleSelected} style={{ backgroundColor: pkmData.selected? '#970000' : 'green'}}>
+                {pkmData.selected? 'Remove' : 'Add'}
+            </div>
             <div className={` ${(pkmData.selected || hovered) && 'shake-animation'}`}>
                 <img src={pkmData.png} width="150" height="180"/>
             </div>
