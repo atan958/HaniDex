46ac8dd991f1cd26af89ea23c4774a4563d0c3ea
Angelo <atan958@aucklanduni.ac.nz>
2021-12-30 17:05:54 +1300

    Renamed pkmSet to pkmItemsIndex to denote the current content index.

--- src/js/Components/pokemonGrid/PkmGrid.js
+++ src/js/Components/pokemonGrid/PkmGrid.js
@@ -19,7 +19,7 @@ const PkmGrid = ({ pkmDataList, loadingPkm }) => {
     / Sets the Pokemon list navigation index back to 0 whenever something is typed into the search bar
     */
     useEffect(() => {
-        setPkmSet(0);
+        setPkmItemsIndex(0);
     }, [search]);
 
     /*
@@ -119,25 +119,25 @@ const PkmGrid = ({ pkmDataList, loadingPkm }) => {
     /*
     / *** TO BE DOCUMENTED ***
     */
-    const [pkmSet, setPkmSet] = useState(0);
+    const [pkmItemsIndex, setPkmItemsIndex] = useState(0);
     /*
     / Used to increase/decrease the value of pkmSet state
     */
-    const incPkmSubset = () => { setPkmSet(pkmSet + 1); }
-    const decPkmSubset = () => { setPkmSet(pkmSet - 1); }
+    const incPkmSubset = () => { setPkmItemsIndex(pkmItemsIndex + 1); }
+    const decPkmSubset = () => { setPkmItemsIndex(pkmItemsIndex - 1); }
 
     /*
     / *** TO BE DOCUMENTED ***
     */
-    const showPrevBtn = pkmSet > 0;
-    const showNextBtn = (filteredPkmList.length / 16) > (pkmSet + 1);
+    const showPrevBtn = pkmItemsIndex > 0;
+    const showNextBtn = (filteredPkmList.length / 16) > (pkmItemsIndex + 1);
 
     /*
     / Filters the list of Pokemon by those which match what's typed on the search bar
     / 
     / Note: Maybe move this functionality into the PkmContainer component instead?
     */
-    let shownPkmList = filteredPkmList.slice(0 + (pkmSet*16),16 + (pkmSet*16));
+    let shownPkmList = filteredPkmList.slice(0 + (pkmItemsIndex*16),16 + (pkmItemsIndex*16));
 
     return (
         <>
