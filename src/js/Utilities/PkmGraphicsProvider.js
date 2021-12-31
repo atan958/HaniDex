99e8d7709cebd13d043b406a74c6d0899ff6ffcb
Angelo <atan958@aucklanduni.ac.nz>
2022-01-01 11:57:48 +1300

    Added some more png fixes to missing Pokemon forms.

--- src/js/Utilities/PkmGraphicsProvider.js
+++ src/js/Utilities/PkmGraphicsProvider.js
@@ -8,11 +8,19 @@ const providePkmPng = (pkmName) => {
     }
     catch(e) {
         pkmPng = require(`../../pokemon-assets/assets/img/pokemon/missingno.png`);
+        
+        /*
+        / Fix for gendered Pokemon (Note: Works for both genders)
+        */
+        if(pkmName.slice(pkmName.length-4, pkmName.length) === 'male') {
+            let genderedPkmName = pkmName.slice(0, pkmName.length-5);
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${genderedPkmName}.png`);
+        }
 
         /*
         / Fix for Deoxys forms
         */
-        if(pkmName == 'deoxys-normal') {
+        if(pkmName === 'deoxys-normal') {
             let defaultName = 'deoxys';
             pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
         }
@@ -20,7 +28,7 @@ const providePkmPng = (pkmName) => {
         /*
         / Fix for Wormadam forms
         */
-        if(pkmName == 'wormadam-plant') {
+        if(pkmName === 'wormadam-plant') {
             let defaultName = 'wormadam';
             pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
         }
@@ -28,7 +36,7 @@ const providePkmPng = (pkmName) => {
         /*
         / Fix for Giratina forms
         */
-        if(pkmName == 'giratina-altered') {
+        if(pkmName === 'giratina-altered') {
             let defaultName = 'giratina';
             pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
         }
@@ -36,7 +44,7 @@ const providePkmPng = (pkmName) => {
         /*
         / Fix for Shaymin forms
         */
-        if(pkmName == 'shaymin-land') {
+        if(pkmName === 'shaymin-land') {
             let defaultName = 'shaymin';
             pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
         }
@@ -44,13 +52,13 @@ const providePkmPng = (pkmName) => {
         /*
         / Fix for Basculin forms
         */
-        if(pkmName == 'basculin-red-striped') {
+        if(pkmName === 'basculin-red-striped') {
             let defaultName = 'basculin';
             pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
         }
 
         /*
-        / Fix for Darmanitan forms
+        / Fix for Darmanitan forms => Note: Over-complicated
         */
         if(pkmName.includes('darmanitan')) {
             let defaultName = 'darmanitan';
@@ -72,13 +80,145 @@ const providePkmPng = (pkmName) => {
         }
 
         /*
-        / Gets the correct png for gendered Pokemon (Note: Works for both genders)
+        / Fix for Keldeo forms
         */
-        if(pkmName.slice(pkmName.length-4, pkmName.length) == 'male') {
-            let genderedPkmName = pkmName.slice(0, pkmName.length-5);
-            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${genderedPkmName}.png`);
+        if(pkmName === 'keldeo-ordinary') {
+            let defaultName = 'keldeo';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
         }
 
+        /*
+        / Fix for Zygarde forms
+        */
+        if(pkmName === 'zygarde-50') {
+            let defaultName = 'zygarde';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Meloetta forms
+        */
+        if(pkmName === 'meloetta-aria') {
+            let defaultName = 'meloetta';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Minior forms
+        */
+        if(pkmName.includes('minior') && pkmName.includes('meteor')) {
+            let defaultName = 'minior';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Gourgeist forms
+        */
+        if(pkmName === 'gourgeist-average') {
+            let defaultName = 'gourgeist';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Pumpkaboo forms
+        */
+        if(pkmName === 'pumpkaboo-average') {
+            let defaultName = 'pumpkaboo';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Aegislash forms
+        */
+        if(pkmName === 'aegislash-shield') {
+            let defaultName = 'aegislash';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Mimikyu forms
+        */
+        if(pkmName === 'mimikyu-disguised') {
+            let defaultName = 'mimikyu';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Mimikyu forms
+        */
+        if(pkmName === 'greninja-battle-bond') {
+            let defaultName = 'greninja-ash';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Oricorio forms
+        */
+        if(pkmName === 'oricorio-baile') {
+            let defaultName = 'oricorio';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Lycanroc forms
+        */
+        if(pkmName === 'lycanroc-midday') {
+            let defaultName = 'lycanroc';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Wishiwashi forms
+        */
+        if(pkmName === 'wishiwashi-solo') {
+            let defaultName = 'wishiwashi';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+        
+        /*
+        / Fix for Toxtricity forms
+        */
+        if(pkmName.includes('toxtricity-amped')) {
+            let defaultName = 'toxtricity';
+            let gmax = '-gmax';
+            if(pkmName.includes(gmax)) { defaultName += gmax }
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Eiscue forms
+        */
+        if(pkmName.includes('eiscue-ice')) {
+            let defaultName = 'eiscue';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+        
+        /*
+        / Fix for Urshifu forms
+        */
+        if(pkmName.includes('urshifu')) {
+            let defaultName = 'urshifu';
+            let gmax = '-gmax'
+            if(pkmName.includes(gmax)) { defaultName += gmax }
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Z-hero forms
+        */
+        if(pkmName.includes('-hero')) {
+            let defaultName = pkmName.slice(0, pkmName.length - '-hero'.length);
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+
+        /*
+        / Fix for Z-hero forms
+        */
+        if(pkmName.includes('totem')) {
+            let defaultName = 'unknown-pokemon';
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${defaultName}.png`);
+        }
+    
     }
     return pkmPng;
 }
