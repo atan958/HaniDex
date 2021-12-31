41c69c961fc824c2c1ed1146ed27ea20f08e8656
Angelo <atan958@aucklanduni.ac.nz>
2021-12-31 21:38:23 +1300

    Removed unused pkmImg variable and added a boundary-case for Deoxys in PkmGraphicsProvider.

--- src/js/Utilities/PkmGraphicsProvider.js
+++ src/js/Utilities/PkmGraphicsProvider.js
@@ -2,12 +2,19 @@
 / A service for providing png files of the Pokemon from pokemon-assets
 */
 const providePkmPng = (pkmName) => {
-    let pkmPng, pkmImg;
+    let pkmPng;
     try {
         pkmPng = require(`../../pokemon-assets/assets/img/pokemon/${pkmName}.png`);
     }
     catch(e) {
         pkmPng = require(`../../pokemon-assets/assets/img/pokemon/missingno.png`);
+
+        /*
+        / Deoxys-normal is non-existant in pokemon-assets img folder
+        */
+        if(pkmName == 'deoxys-normal') {
+            pkmPng = require(`../../pokemon-assets/assets/img/pokemon/deoxys.png`);
+        }
     }
     return pkmPng;
 }
