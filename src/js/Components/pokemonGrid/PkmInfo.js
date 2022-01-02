9d692c6eb1de9213ac3ad934c6888b8d25115b2d
Angelo <atan958@aucklanduni.ac.nz>
2022-01-02 18:40:58 +1300

    Successfully removed the \f escape character from the Pokemon's text entries.

--- src/js/Components/pokemonGrid/PkmInfo.js
+++ src/js/Components/pokemonGrid/PkmInfo.js
@@ -9,17 +9,22 @@ const PkmInfo = ({pkmToShow, hideInfo}) => {
     const [showShiny, setShowShiny] = useState(false);
     const toggleShowShiny = () => { setShowShiny(!showShiny) };
 
+    /*
+    / Capitalizes the first letter of every name of the Pokemon
+    */
     const displayName = pkmToShow.name.pkmItem.split(' ').map((namePart) => {
         return namePart.charAt(0).toUpperCase() + namePart.slice(1, namePart.length);
     }).join(' ');
+
+    const descToShow = rmvEscChars(pkmToShow.desc.default);
 
     return (
     <div className="pkmInfo-overlay-container">
         <div className="pkmInfo-content-container content-centered fasterFadeIn-half-animation">
             <div className="pkmInfo-content">
-                <div className="pkmInfo-desc">
+                <div className="pkmInfo-desc fadeIn-animation">
                     <p>
-                        {pkmToShow.desc.default}
+                        {descToShow}
                     </p>
                 </div>
                 <div className="overlay-closeBtn-container">
@@ -40,3 +45,17 @@ const PkmInfo = ({pkmToShow, hideInfo}) => {
 }
 
 export default PkmInfo
+
+/*
+/ Removes the escape character \f from some of the Pokemon text entries which shows up as a black blotch
+*/
+const rmvEscChars = (pkmDesc) => {
+    let descDefault = pkmDesc;
+    if(descDefault.includes('\f')) {
+        let i = descDefault.indexOf('\f');
+        let descArr = descDefault.split('');
+        descArr.splice(i,1,' ');
+        descDefault = descArr.join('');
+    }
+    return descDefault;
+}
